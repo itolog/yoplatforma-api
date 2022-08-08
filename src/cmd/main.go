aa69621b92ed6bf089fecc8a7ac78e445658e65a
itolog <paukanlove@gmail.com>
2022-08-08 14:46:19 +0300

    add os files to gitignore

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Worxld!")
+		return c.SendString("Hello, World!")
 	})
 
 	err := app.Listen(":8000")
